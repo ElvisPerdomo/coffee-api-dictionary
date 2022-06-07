4f6cff3ecb96009ad97fea707b2a690890b28f2a
Elvis Perdomo <Perdomoelvis@hotmail.com>
2022-06-07 18:30:07 -0400

    adding coffee api dictionary to heroku

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+let coffee = {
+    'monte real': {
+        'flavor': 'chocolate',
+        'acidity': '5/10',
+        'rarity': 'uncommon',
+        'roast': 'dark',
+        'profile': 'bold'
+
+    },
+ 
+    'bustelo': {
+        'flavor': 'regular',
+        'acidity': '6/10',
+        'rarity': 'common',
+        'roast': 'dark',
+        'profile': 'bold'
+
+    },
+
+    'folgers': {
+        'flavor': 'hazelnut',
+        'acidity': '3/10',
+        'rarity': 'common',
+        'roast': 'medium',
+        'profile': 'medium'
+
+    },
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    const coffeeName = request.params.name.toLowerCase()
+    if(coffee[coffeeName]){
+        response.json(coffee[coffeeName])
+    }else{
+        response.json(coffee['unknown'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
